328be61d982b133f11f148a623ab2941c3d7bbfe
Alejandro González Parra <agonzalezp@iesrafaelalberti.es>
2022-01-19 18:29:39 +0100

    Terminado con Centrar el texto

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -12,8 +12,14 @@ fun imprimirCapasInf(edad: Int,capas:Int){
     repeat(capas){ println("@".repeat(edad)) }
 }
 
-fun imprimirMensaje(mensaje:String){
-    println("\"-._.-\"".repeat(5) +"\n" + mensaje + "\n" + "\"-._.-\"".repeat(5))
+fun imprimirMensaje(mensaje:String, edad:Int){
+    val espaciosBlancos = (edad - mensaje.length)/2
+    var bandera = ""
+    while (bandera.length < (mensaje.length + espaciosBlancos*2)){ bandera += ("\"-._.-\"") }
+    println(bandera)
+    println(" ".repeat(espaciosBlancos) + mensaje + " ".repeat(espaciosBlancos))
+    println(bandera)
+
 }
 
 
@@ -35,16 +41,16 @@ fun main() {
     } catch (_:Exception) { println("Las capas se introdujo de forma incorrecta."); return }
     if(edad in 1..98 && capas>0) {
         print("\n")
-        imprimirMensaje(mensaje)
+        imprimirMensaje(mensaje,edad)
         print("\n")
         imprimirVelas(edad)
         imprimirCapaSup(edad)
         imprimirCapasInf(edad,capas)
         print("\n")
-        imprimirMensaje(mensaje)
+        imprimirMensaje(mensaje,edad)
     } else {
         if(edad !in 1..98) println("La edad está fuera del rango permitido.")
-        if(edad <= 0) println("Las capas no pueden ser 0 o menor de 0.")
+        if(edad <= 0) println("Las capas n  o pueden ser 0 o menor de 0.")
     }
 
 }
